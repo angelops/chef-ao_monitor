c2d17c7e8fef62976be29929b74f622208286599
Justin Alan Ryan <juryan@ziprealty.com>
2015-09-10 11:29:16 -0700

    use nginx for nagios, bump cookbook version.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 node.default['nagios']['multi_environment_monitoring'] = true
+node.default['nagios']['server']['web_server'] = 'nginx'
 node.default['apache']['mpm'] = 'prefork'
 default['zip_monitor']['additional_packages'] = %w{nagcon nagios-plugins nagios-plugins-contrib}
