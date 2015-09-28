839f4b66c29e8cdce5f40d9e181f7e54ad83f0bc
Justin Alan Ryan <juryan@ziprealty.com>
2015-09-28 16:41:33 -0700

    set nagios/conf/interval_length to 1 by default.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 #node.default['nagios']['server']['web_server'] = 'nginx'
+default['nagios']['conf']['interval_length'] = 1
 node.default['apache']['mpm'] = 'prefork'
 default['zip_monitor']['additional_packages'] = %w{nagcon nagios-plugins nagios-plugins-contrib}
